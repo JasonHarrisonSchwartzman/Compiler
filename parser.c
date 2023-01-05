412424614bec043f93a8f67525e875387cd4908b
Jason Schwartzman <megaslowbro1@gmail.com>
2023-01-05 11:28:29 -0500

    Update parser.c

--- parser.c
+++ parser.c
@@ -121,31 +121,3 @@ int parse() {
 	}
 	return -1;
 }
-
-int main(int argc, char *argv[]) {
-	scanner(argc, argv);
-	initializeRules();
-	createInstanceAndVarTokens();
-	printRules();
-	initializeRules();
-	createInstanceAndVarTokens();
-	printRules();
-	initializeInstances();
-	printTokens();
-	int result = parse();
-	if (result == 1) {
-		printf("SUCCESS!\n");
-	}
-	else if (result == 0) {
-	 	printf("FAILURE\n");
-	}
-	else {
-		printf("ODD FAILURE\n");
-	}
-
-
-	freeRules();
-	freeInstanceAndVarTokens();
-	// //freeTokens();seg fault
-	freeStack();
-}
