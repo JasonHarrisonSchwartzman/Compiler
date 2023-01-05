421cd903fa2ea3f4fc9e5758cc161791113b4ed4
Jason Schwartzman <megaslowbro1@gmail.com>
2023-01-04 21:55:34 -0500

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,6 +1,17 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+struct Rule {
+	token_t var;
+	token_t *symbols;//tokens and variables
+	int length;
+} Rule;
+
+struct Instance {
+	struct Action actions[NUM_ACTIONS];
+	int gotoAction[NUM_GOTO];
+};
+
 typedef enum Step{
 	STEP_ERROR,
 	STEP_REDUCE,
