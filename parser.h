e26dd48816dc0d3cc45392514f226d6f17e103e2
Jason Schwartzman <megaslowbro1@gmail.com>
2023-01-04 21:43:17 -0500

    Update parser.h

--- parser.h
+++ parser.h
@@ -1,6 +1,17 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+typedef enum Step{
+	STEP_ERROR,
+	STEP_REDUCE,
+	STEP_SHIFT,
+	STEP_ACCEPT
+}Step;
+
+struct Action {
+	Step step;
+	int instance;//or rule in grammar
+};
 
 
 #endif
