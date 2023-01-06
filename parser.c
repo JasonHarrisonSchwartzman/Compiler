290802ddbfd2a34dcb4f07634dfd5aac241560db
Jason Schwartzman <megaslowbro1@gmail.com>
2023-01-06 08:55:01 -0500

    Update parser.c

--- parser.c
+++ parser.c
@@ -4,27 +4,6 @@
 #include "parserinit.c"
 #include "parser.h"
 
-struct StackItem {
-	token_t instance;
-	Token token;
-	token_t var;
-	void *ptr;
-} StackItem;
-
-
-typedef enum type_t {
-	INT,
-	LONG,
-	SHORT,
-	CHAR,
-	DOUBLE
-} type_t;
-
-typedef enum signed_t {
-	SIGNED,
-	UNSIGNED
-} signed_t;
-
 void push(Token *token) {
 	stack = realloc(stack,sizeof(Token) * (stackTopPointer + 2));
 	stack[++stackTopPointer] = token;
