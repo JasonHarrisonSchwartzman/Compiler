0e7efa750ca13ccbc891767c7b85020670db70d2
Jason Schwartzman <megaslowbro1@gmail.com>
2023-01-06 09:15:49 -0500

    Update parser.h

--- parser.h
+++ parser.h
@@ -6,27 +6,6 @@
 #define NUM_INSTANCES 300
 #define TOTAL_TOKENS 50
 
-struct StackItem {
-	token_t instance;
-	Token token;
-	token_t var;
-	void *ptr;
-} StackItem;
-
-
-typedef enum type_t {
-	INT,
-	LONG,
-	SHORT,
-	CHAR,
-	DOUBLE
-} type_t;
-
-typedef enum signed_t {
-	SIGNED,
-	UNSIGNED
-} signed_t;
-
 
 typedef enum Step{
 	STEP_ERROR,
