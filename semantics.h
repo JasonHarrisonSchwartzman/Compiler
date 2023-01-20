0750d440ef846ec895fdfea13c9d34c31081880b
Jason Schwartzman <megaslowbro1@gmail.com>
2023-01-20 12:18:28 -0500

    Update semantics.h

--- semantics.h
+++ semantics.h
@@ -1,19 +1,1 @@
-#ifndef SEMANTICS_H
-#define SEMANTICS_H
-#include "syntaxtree.h"
 
-typedef enum symbol_t {
-	SYMBOL_GLOBAL,
-	SYMBOL_LOCAL,
-	SYMBOL_PARAM
-} symbol_t;
-
-struct Symbol {
-	char *name;
-	struct Type *type;
-	symbol_t sym;
-	dec_t dec;
-	struct Symbol *next;
-} Symbol;
-
-#endif
