6e8a74bc011479c1c99e40834d76f2905d397f0d
Jason Schwartzman <jasonschwartzman@Jasons-Air-2.lan>
2023-08-09 22:55:18 -0400

    added more dag types

--- ir.c
+++ ir.c
@@ -1,14 +1,40 @@
 typedef enum {
     DAG_ASSIGN,
     DAG_DEREF,
+    DAG_REF,
+
     DAG_ADD,
     DAG_MULT,
     DAG_DIV,
+    DAG_MINUS,
+    DAG_MOD,
+    DAG_AND,
+    DAG_OR,
+    DAG_NOTEQUAL,
+    DAG_BITAND,
+    DAG_BITOR,
+    DAG_BITXOR,
+    DAG_EQUAL,
+    DAG_LESSEQUAL,
+    DAG_LESS,
+    DAG_GREAT,
+    DAG_GREATEQUAL,
 
     DAG_NAME,
+
     DAG_DOUBLE_VAL,
-    DAG_INT_VAL
+    DAG_INT_VAL,
+    DAG_SHORT_VAL,
+    DAG_LONG_VAL,
+    DAG_CHAR_VAL,
+    DAG_UDOUBLE_VAL,
+    DAG_UINT_VAL,
+    DAG_USHORT_VAL,
+    DAG_ULONG_VAL,
+    DAG_UCHAR_VAL,
 } dag_kind_t;
+//DAG constructed by post-order traversal of the AST
+//search nodes to re-use
 struct dag_node {
     dag_kind_t kind;
     struct dag_node *left;
