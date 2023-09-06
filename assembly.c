63470f6fe8574249f4ab5e9ee61290c96e567fe6
Jason Schwartzman <megaslowbro1@gmail.com>
2023-09-05 20:38:58 -0400

    added preliminary functions

--- assembly.c
+++ assembly.c
@@ -8,3 +8,46 @@
 
 extern struct quad **quads;
 extern int numQuads;
+
+struct scratch_register {
+    char *name;
+    int inUse;
+} scratch_register;
+
+struct scratch_register registers[] = {
+    {"%rbx",0},
+    {"%r10",0},
+    {"%r11",0},
+    {"%r12",0},
+    {"%r13",0},
+    {"%r14",0},
+    {"%r15",0},
+};
+int numRegisters = 6;
+
+/**
+ * Finds unused register and makes it in use
+*/
+int scratch_alloc() {
+    for (int i = 0; i < numRegisters; i++) {
+        if (!registers[i].inUse) {
+            registers[i].inUse = 1;
+            return i;
+        }
+    }
+    return -1;
+}
+
+/**
+ * Marks register as free
+*/
+void scratch_free(int reg) {
+    registers[reg].inUse = 0;
+}
+
+/**
+ * Returns names of register
+*/
+const char *scratch_name(int reg) {
+    return registers[reg].name;
+}
