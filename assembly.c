93ab0b4a88f48d8e128b9862547ec146b56ff802
Jason Schwartzman <megaslowbro1@gmail.com>
2023-09-18 13:43:06 -0400

    added mod

--- assembly.c
+++ assembly.c
@@ -340,7 +340,7 @@ void expr_codegen(struct quad *quad) {
                 movRax = concatenateStrings("MOVQ %rax, ", scratch_name(raxTemp));
                 addCode(movRax);
             }
-            if (registers[1].inUse) {
+            if (registers[3].inUse) {
                 rdxUsed = 1;
                 rdxTemp = scratch_alloc();
                 movRdx = concatenateStrings("MOVQ %rdx, ", scratch_name(rdxTemp));
@@ -378,8 +378,57 @@ void expr_codegen(struct quad *quad) {
                 scratch_free(rdxTemp);
             }
             break; }
-        case OP_MOD:
-            break;
+        case OP_MOD: {
+            int raxUsed = 0;
+            int rdxUsed = 0;
+            int raxTemp;
+            char *movRax;
+            int rdxTemp;
+            char *movRdx;
+            if (registers[0].inUse) {
+                raxUsed = 1;
+                raxTemp = scratch_alloc();
+                movRax = concatenateStrings("MOVQ %rax, ", scratch_name(raxTemp));
+                addCode(movRax);
+            }
+            if (registers[3].inUse) {
+                rdxUsed = 1;
+                rdxTemp = scratch_alloc();
+                movRdx = concatenateStrings("MOVQ %rdx, ", scratch_name(rdxTemp));
+                addCode(movRdx);
+            }
+
+            char *dividend = symbolToOperand(quad,quad->arg1);
+            char *movDividend = concatenateStrings("MOVQ ", dividend);
+            movDividend = concatenateStrings(movDividend,", %rax");
+            addCode(movDividend);
+            char *divisor = symbolToOperand(quad,quad->arg2);
+            char *movDivisor = concatenateStrings("MOVQ ", divisor);
+            int divisorReg = scratch_alloc();
+            movDivisor = concatenateStrings(movDivisor,", ");
+            movDivisor = concatenateStrings(movDivisor,scratch_name(divisorReg));
+            addCode(movDivisor);
+            char *div = concatenateStrings("IDIVQ ",scratch_name(divisorReg));
+            addCode(div);
+            int reg = scratch_alloc();
+            char *movResult = concatenateStrings("MOVQ %rdx, ",scratch_name(reg));
+            addCode(movResult);
+            quad->reg = reg;
+            scratch_free(divisorReg);
+
+            if (raxUsed) {
+                char *movRaxBack = concatenateStrings("MOVQ ", scratch_name(raxTemp));
+                movRaxBack = concatenateStrings(movRaxBack, ", %rax");
+                addCode(movRaxBack);
+                scratch_free(raxTemp);
+            }
+            if (rdxUsed) {
+                char *movRdxBack = concatenateStrings("MOVQ ", scratch_name(rdxTemp));
+                movRdxBack = concatenateStrings(movRdxBack, ", %rdx");
+                addCode(movRdxBack);
+                scratch_free(rdxTemp);
+            }
+            break; }
         case OP_BITAND: {
             int reg1 = move(quad,quad->arg1);
             int reg2 = move(quad,quad->arg2);
